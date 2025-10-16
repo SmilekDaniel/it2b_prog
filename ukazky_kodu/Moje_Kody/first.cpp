8041aa8bd9d1ce16e0dcc3f60307cdf6bd701a55
SmilekDaniel <st026356@student.cichnovabrno.cz>
2025-10-16 11:39:25 +0200

    Stringy

--- ukazky_kodu/Moje_Kody/first.cpp
+++ ukazky_kodu/Moje_Kody/first.cpp
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    print("Ahoj svete");
-    print("Ahoj");
+    char str[25];
+    int konec = 0;
+    printf("Nacti slovo (max 25 pismen)");
+    scanf_s(" %s", str);
+    int i = 0;
+    while(konec < 1){
+        if(str[i] == 0){
+            konec = 1
+        }else{
+            i++
+        }
+    }
     return 0;
 }
